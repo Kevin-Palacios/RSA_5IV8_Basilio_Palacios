99b1b6ca435b0fde0eed648396e93beddba42b44
unknown <kapemonito14@homail.com>
2020-11-30 01:04:44 -0600

    RSA

---
+++ src/rsa/RSA.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rsa;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import static rsa.RSA_.desencriptarXD;
+
+/**
+ *
+ * @author Gerardo
+ */
+public class RSA {
+
+    public static void main(String[] args) throws IOException {
+
+//        BigInteger[] mensaje;
+//        BigInteger d;
+//        BigInteger n;
+//        String mensaje_ = "";
+//
+//        String menSaje = "Chinga tu madre Angela";
+//
+//        RSA_ unu = new RSA_(10, menSaje);
+//
+//        mensaje = unu.cifradoOb;
+//        d = unu.d;
+//        n = unu.n;
+//        System.out.println("mensaje = " + mensaje);
+//
+//        for (int i = 0; i < unu.getUnu(); i++) {
+//            mensaje_ += " " + mensaje[i];
+//        }
+//        System.out.println("mensaje_CIFRADO = " + mensaje_);
+//        System.out.println("UNUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUuu");
+//        System.out.println(mensaje.length);
+//        System.out.println("Mensaje desifrado = " + unu.desencriptar(mensaje));
+//
+//        String mensajekl = desencriptarXD(mensaje, d, n);
+//        System.out.println("mensajekl = " + mensajekl);
+        Ventana ventana = new Ventana();
+        ventana.setVisible(true);
+//        String sCadena = "496324 55259 144583 208497 178962 202121 84173 98051 204374 84173 308228 202121 400119 384194 460954 84173 168239 208497 178962 460954 329607 202121 ";
+//        String cadena2 = sCadena;
+//        char[] aCaracteres;
+//        aCaracteres = sCadena.toCharArray();
+//        int longi = 1;
+//        int xd = 1;
+////        int separacion = cadena2.indexOf(" ") + 1;
+//        for (int i = 0; xd > 0; i++) {
+//            cadena2 = cadena2.substring(cadena2.indexOf(" ") + 1);
+//            longi = longi + 1;
+//            if (cadena2.length() <= sCadena.indexOf(" ")) {
+//                xd = -1;
+//            }
+//        }
+//        cadena2 = sCadena;
+//        BigInteger[] ka = new BigInteger[longi - 1];
+//        for (int i = 0; i < longi - 1; i++) {
+//            ka[i] = BigInteger.valueOf(Integer.parseInt(cadena2.substring(0, cadena2.indexOf(" "))));
+//            cadena2 = cadena2.substring(cadena2.indexOf(" ") + 1);
+//        }
+
+
+    }
+}
